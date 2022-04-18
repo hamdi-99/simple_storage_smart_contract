4220f83d7bf525909c2f4e0fab9727102abad3fd
hamdi <hamdiharaketi@insat.u-carthage.tn>
2022-04-18 04:32:48 +0100

    adding comments to the code

--- deploy.py
+++ deploy.py
@@ -9,26 +9,31 @@ load_dotenv()
 with open("./SimpleStorage.sol", "r") as file:
     simple_storage_file = file.read()
 
-
+# Getting the contract abi
 abi, bytecode = contract_abi(simple_storage_file, "SimpleStorage.sol", "SimpleStorage")
 
+# connecting web3 and setting the wanted variables
 w3 = Web3(Web3.HTTPProvider(os.getenv("URL")))
 chain_id = int(os.getenv("CHAIN_ID"))
 address = os.getenv("ADDRESS")
 
-
+# initializing the contract as an object
 simple_storage = w3.eth.contract(abi=abi, bytecode=bytecode)
 
+# getting the nonce which here is the number of the transactions that are made
 nonce = w3.eth.getTransactionCount(address)
 
+# building the transaction (the transaction here is the creation of the contract)
 transaction = simple_storage.constructor().buildTransaction(
     {"chainId": chain_id, "from": address, "gasPrice": w3.eth.gas_price, "nonce": nonce}
 )
 
+# signing the transaction
 signed_transaction = w3.eth.account.sign_transaction(
     transaction, private_key=os.getenv("PRIVATE_KEY")
 )
 
+# Deploying our contract by sending the transaction
 print("Deploying contract ...")
 transaction_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
 print("Waiting for transaction to finish...")
--- load_contract.py
+++ load_contract.py
@@ -15,15 +15,17 @@ w3 = Web3(Web3.HTTPProvider(os.getenv("URL")))
 chain_id = int(os.getenv("CHAIN_ID"))
 address = os.getenv("ADDRESS")
 
+# loading the contract from our blockchain network
 simple_storage = w3.eth.contract(
     address="0x86C0ca19A9356E11608aDf799e32aAC8C889A4C0", abi=abi
 )
 
 nonce = w3.eth.getTransactionCount(address)
 
-
+# Calling a view function from our loaded contract
 print(f"Initial Stored Value {simple_storage.functions.retrieve().call()}")
 
+# Updating contract by calling a function which change the state of our contract which means building a transaction
 transaction = simple_storage.functions.store(30).buildTransaction(
     {
         "chainId": chain_id,
@@ -33,10 +35,12 @@ transaction = simple_storage.functions.store(30).buildTransaction(
     }
 )
 
+# signing the transaction
 signed_transaction = w3.eth.account.sign_transaction(
     transaction, private_key=os.getenv("PRIVATE_KEY")
 )
 
+# Updating our contract by sending the transaction
 transaction_hash = w3.eth.send_raw_transaction(signed_transaction.rawTransaction)
 
 print("Updating stored Value...")
